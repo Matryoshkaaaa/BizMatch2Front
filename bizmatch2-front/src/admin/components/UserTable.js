2eb43b4b02db0f59b5dc5f806c130dd2a6bb642e
Shin Dong Won <eastorigin21@gmail.com>
2024-12-04 16:15:24 +0900

    멤버 리뷰 모든 기능 구현 완료/sdw

--- bizmatch2-front/src/admin/components/UserTable.js
+++ bizmatch2-front/src/admin/components/UserTable.js
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from "react-redux";
-// import { approveMember, removeMember } from "./ActionButtons";
 import SearchMembers from "./SearchMembers";
 import { useEffect } from "react";
 import {

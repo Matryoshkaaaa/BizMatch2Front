6fbeb723533ae8a745fd29fc9e4b3864f32cbf96
JiChaeYoung <103018258+JiChaeYoung@users.noreply.github.com>
2024-12-04 18:21:07 +0900

    Delete src directory

--- src/components/member/Introduction.js
+++
@@ -1,8 +0,0 @@
-export default function Introduction({ companyVO }) {
-  return (
-    <div className="introduction" id="introduction">
-      회사 소개
-      <div className="introduction-content">{companyVO.cmpnyIntr}</div>
-    </div>
-  );
-}
--- src/pages/RegisterPage.js
+++
@@ -1,9 +0,0 @@
-import SignupCompany from "../components/member/SignUp_Company";
-
-export default function RegisterPage() {
-  return (
-    <>
-      <SignupCompany />
-    </>
-  );
-}

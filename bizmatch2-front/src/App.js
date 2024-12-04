527385e087c6fc2a8773cce880e03b8b50cb084f
JiChaeYoung <wlcodud7@naver.com>
2024-12-04 18:22:43 +0900

    temp

--- bizmatch2-front/src/App.js
+++ bizmatch2-front/src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { AppProvider } from "./admin/features/users/userSlice";
 import AppRouterProvider from "./routers/AppRouters";
-/**
- *
- * @returns
- */
+
 export default function App() {
   return (
     <AppProvider>

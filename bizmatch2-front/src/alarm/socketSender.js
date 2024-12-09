1c601ab410c1147ae4462683b01ff8a484993e42
Matryoshkaaaa <tthh0518@naver.com>
2024-12-09 17:01:28 +0900

    th

--- bizmatch2-front/src/alarm/socketSender.js
+++ bizmatch2-front/src/alarm/socketSender.js
@@ -6,12 +6,15 @@ var socket = undefined;
 socket = new SockJS("http://localhost:8080/ws");
 
 socket.onopen = () => {
-  console.log("소켓 연결 성공");
+  if (sessionStorage.getItem("info") !== null) {
+    const email = JSON.parse(sessionStorage.getItem("info")).emilAddr;
+    signinAlarmSender(email);
+  }
 };
 
 export const signinAlarmSender = (email) => {
   var sendMessage = {
-    email,
+    email: email,
     action: "LOGIN",
     message: "로그인",
     url: null,

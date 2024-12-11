c09fd1399d3266edd2e922a0a0f48f732da01e25
Shin Dong Won <eastorigin21@gmail.com>
2024-12-11 16:26:31 +0900

    포트폴리오 수정하면 모달에 즉시 반영 완료/sdw

--- bizmatch2-front/src/components/ui/PortfolioModal.js
+++ bizmatch2-front/src/components/ui/PortfolioModal.js
@@ -57,6 +57,7 @@ export default function PortfolioModal({ mbrPrtflId, onClose }) {
     dispatch(updatePortfolioThunk(mbrPrtflId, editData))
       .then(() => {
         alert("포트폴리오가 성공적으로 수정되었습니다.");
+        dispatch(getOnePortfolioThunk(mbrPrtflId));
         setEditMode(false); // 수정 모드 종료
       })
       .catch((error) => {
@@ -125,8 +126,8 @@ export default function PortfolioModal({ mbrPrtflId, onClose }) {
                   type="text"
                   name="mbrPrtflTtl"
                   onChange={handleEditChange}
-                  placeholder="프로젝트명"
                   ref={mbrPrtflTtlRef}
+                  defaultValue={portfolioDetails.mbrPrtflTtl}
                   required
                 />
               </div>
@@ -134,8 +135,8 @@ export default function PortfolioModal({ mbrPrtflId, onClose }) {
                 <textarea
                   name="mbrPrtflText"
                   onChange={handleEditChange}
-                  placeholder="프로젝트 상세"
                   ref={mbrPrtflTextRef}
+                  defaultValue={portfolioDetails.mbrPrtflText}
                   required
                 ></textarea>
               </div>

2ece78432df6aa7dfef9f8c6e687ae1790d2f1a3
Shin Dong Won <eastorigin21@gmail.com>
2024-12-16 08:40:37 +0900

    로그인 모달 esc 및 navlink 스크롤 초기화 전역 적용/sdw

--- bizmatch2-front/src/components/ui/PaymentLayout.js
+++ bizmatch2-front/src/components/ui/PaymentLayout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AfterLoginHeader from "../main/AfterLoginHeader";
+import ScrollToTop from "../main/ScrollToTop";
 
 export default function PaymentLayout() {
   return (
     <>
+      <ScrollToTop />
       <AfterLoginHeader />
       <Outlet />
     </>

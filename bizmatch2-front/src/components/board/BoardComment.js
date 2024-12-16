168be5b8d15567ce557ff12ba26a720a64a1f576
HSB-ktds-24 <bum7795@gmail.com>
2024-12-16 17:09:41 +0900

    저장 습관화

--- bizmatch2-front/src/components/board/BoardComment.js
+++ bizmatch2-front/src/components/board/BoardComment.js
@@ -7,7 +7,7 @@ import {
   removeBoardComment,
   fetchAllBoardComments,
 } from "../../stores/thunks/boardCommentThunk";
-import BoardViewStyle from "./BoardView.module.css";
+import CommentStyle from "./CommentDefualt.module.css";
 export default function BoardComment({ data, boardId }) {
   const [isReplying, setIsReplying] = useState(false); // 답글 입력창 표시 여부
   const [isEditing, setIsEditing] = useState(false); // 수정 입력창 표시 여부
@@ -56,25 +56,25 @@ export default function BoardComment({ data, boardId }) {
     <>
       {data.isDlt === 0 ? (
         <div
-          className={BoardViewStyle.oneComment}
+          className={CommentStyle.commentBox}
           style={{ marginLeft: `${(data.lv - 1) * 1.2}rem` }}
         >
-          <div className={BoardViewStyle.commentUpperside}>
-            <div className={BoardViewStyle.commentLeftPart}>
-              <div className={BoardViewStyle.name}>
+          <div className={CommentStyle.commentUpperside}>
+            <div className={CommentStyle.commentLeftPart}>
+              <div className={CommentStyle.name}>
                 {data.mbrNm} ({data.athrId})
               </div>
               {!isEditing ? (
-                <div className={BoardViewStyle.content}>{data.cmmntCntnt}</div>
+                <div className={CommentStyle.content}>{data.cmmntCntnt}</div>
               ) : (
-                <div className={BoardViewStyle.modifyWriteBox}>
+                <div className={CommentStyle.writeBox}>
                   <textarea
                     ref={modifyRef}
                     defaultValue={data.cmmntCntnt}
-                    className={BoardViewStyle.modifyText}
+                    className={CommentStyle.modifyText}
                   />
                   <button
-                    className={BoardViewStyle.submitBtn}
+                    className={CommentStyle.submitBtn}
                     onClick={modifyCommentHandler}
                   >
                     등록
@@ -82,24 +82,21 @@ export default function BoardComment({ data, boardId }) {
                 </div>
               )}
             </div>
-            <div className={BoardViewStyle.commentRightPart}>
-              <div className={BoardViewStyle.dateBox}>
-                <div className={BoardViewStyle.createDate}>{data.lstModDt}</div>
+            <div className={CommentStyle.commentRightPart}>
+              <div className={CommentStyle.dateBox}>
+                <div className={CommentStyle.createDate}>{data.lstModDt}</div>
               </div>
-              <div
-                className={BoardViewStyle.functionLine}
-                data-id={data.cmmntId}
-              >
+              <div className={CommentStyle.functionLine} data-id={data.cmmntId}>
                 {data.athrId === currUserEmail && (
                   <>
                     <input
-                      className={BoardViewStyle.modifyCommentBtn}
+                      className={CommentStyle.modifyCommentBtn}
                       type="button"
                       value="수정"
                       onClick={() => setIsEditing(!isEditing)} // 수정창 토글
                     />
                     <input
-                      className={BoardViewStyle.deleteCommentBtn}
+                      className={CommentStyle.deleteCommentBtn}
                       type="button"
                       value="삭제"
                       onClick={deleteCommentHandler}
@@ -107,7 +104,7 @@ export default function BoardComment({ data, boardId }) {
                   </>
                 )}
                 <input
-                  className={BoardViewStyle.recommentBtn}
+                  className={CommentStyle.recommentBtn}
                   type="button"
                   value="답글"
                   onClick={() => setIsReplying(!isReplying)} // 답글창 토글
@@ -116,14 +113,14 @@ export default function BoardComment({ data, boardId }) {
             </div>
           </div>
           {isReplying && (
-            <div className={BoardViewStyle.recommentWriteBox}>
+            <div className={CommentStyle.writeBox}>
               <textarea
-                className={BoardViewStyle.recommentText}
+                className={CommentStyle.recommentText}
                 placeholder="답글을 입력하세요"
                 ref={recommentRef}
               />
               <button
-                className={BoardViewStyle.submitBtn}
+                className={CommentStyle.submitBtn}
                 onClick={addReplyHandler}
               >
                 등록
@@ -133,7 +130,7 @@ export default function BoardComment({ data, boardId }) {
         </div>
       ) : (
         <div
-          className={BoardViewStyle.deletedComment}
+          className={CommentStyle.deletedComment}
           style={{ marginLeft: `${(data.lv - 1) * 1.2}rem` }}
         >
           삭제된 댓글입니다.
